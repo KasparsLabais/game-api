71f1c9d184ec8da2669d850c906db924bff4b5f7
kvanags <kvanags@firestormmedia.tv>
2023-09-29 16:18:22 +0100

    update game js to support seperate game room and game instance join function

--- resources/static/js/game.js
+++ resources/static/js/game.js
@@ -10,13 +10,23 @@ socket.on('playerJoined', (data) => {
 });
 
 const joinRoom = (gameToken, repeatCount = 0) => {
-    socket.emit('joinGameInstance', { 'gameToken' : gameToken, 'repeatCount' : repeatCount}, (answer) => {
+    socket.emit('joinRoom', { 'gameToken' : gameToken, 'repeatCount' : repeatCount}, (answer) => {
         if (!answer.status && answer.repeatCount  < 10) {
             setTimeout(() => { console.log('Repeat'); joinRoom(gameToken, answer.repeatCount); }, 1000);
         }
     });
 }
 
+const joinGameInstance = (gameToken, redirectUrl, repeatCount = 0) => {
+    socket.emit('joinGameInstance', { 'gameToken' : gameToken, 'repeatCount' : repeatCount}, (answer) => {
+        if (!answer.status && answer.repeatCount  < 10) {
+            setTimeout(() => { console.log('Repeat'); joinGameInstance(gameToken, redirectUrl, answer.repeatCount); }, 1000);
+        } else {
+            console.log('Redirecting to ', redirectUrl);
+            window.location.href = redirectUrl;
+        }
+    });
+}
 //game instance controller to handle socket io events
 const addGameInstance = (gameToken, gameInstance) => {
     console.log('addGameInstance', gameToken, gameInstance);
