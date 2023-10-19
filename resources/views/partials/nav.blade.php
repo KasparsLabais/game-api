1f55922017ecd53dabeb36fa782c4c7547f456c5
kvanags <kvanags@firestormmedia.tv>
2023-10-19 13:20:35 +0100

    test

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,39 +1,97 @@
+<script>
+
+    const openHamburger = () => {
+        document.getElementById('mobile-nav').classList.remove('hidden');
+    }
+
+    const closeHamburger = () => {
+        document.getElementById('mobile-nav').classList.add('hidden');
+    }
+
+</script>
+
 <div class="flex w-full justify-between">
-    <div class="flex flex-row px-12">
+    <div class="flex flex-row md:px-12">
         <img src="https://via.placeholder.com/65" alt="Logo">
         <div class="flex flex-col justify-center">
             <h1 class="px-2 fira-sans">Party Games</h1>
         </div>
     </div>
     <div class="flex flex-row justify-center raleway">
-        <div class="flex flex-col justify-center">
-            <a href="/" class="px-8 py-2 hover:text-slate-300 hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
-                Home
-            </a>
-        </div>
-        <div class="flex flex-col justify-center">
-            <a href="/games" class="px-8 py-2 hover:text-slate-300 hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
-                Games
-            </a>
-        </div>
-        @if(Auth::check())
-        <div class="flex flex-col justify-center">
-            <a href="/profile" class="px-8 py-2 hover:text-slate-300 hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">Profile</a>
-        </div>
-        @endif
-        @if(Auth::check())
-            <div class="flex">
-                <a href="/logout" class="px-8 py-2 hover:text-slate-300	hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
-                    Logout
+        <div class="hidden md:flex">
+            <div class="flex flex-col justify-center">
+                <a href="/" class="px-8 py-2 hover:text-slate-300 hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
+                    Home
                 </a>
             </div>
-        @else
-            <div class="flex">
-                <a href="/login" class="px-8 py-2 hover:text-slate-300	hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
-                    Login
+            <div class="flex flex-col justify-center">
+                <a href="/games" class="px-8 py-2 hover:text-slate-300 hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
+                    Games
                 </a>
             </div>
-        @endif
+            @if(Auth::check())
+                <div class="flex flex-col justify-center">
+                    <a href="/profile" class="px-8 py-2 hover:text-slate-300 hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">Profile</a>
+                </div>
+            @endif
+            @if(Auth::check())
+                <div class="flex">
+                    <a href="/logout" class="px-8 py-2 hover:text-slate-300	hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
+                        Logout
+                    </a>
+                </div>
+            @else
+                <div class="flex">
+                    <a href="/login" class="px-8 py-2 hover:text-slate-300	hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
+                        Login
+                    </a>
+                </div>
+            @endif
+        </div>
+        <div class="flex flex-col justify-center px-4 md:hidden">
+            <div onclick="openHamburger()">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </div>
+            <div id="mobile-nav" class="hidden flex flex-col absolute right-0 top-0 bg-slate-100 w-3/4 h-full mx-auto">
+                <div class="flex flex-col justify-center" onclick="closeHamburger()">
+                    <span class="border-b border-b-slate-300 px-8 py-2 hover:text-slate-300 hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
+                        <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </span>
+                </div>
+                <div class="flex flex-col justify-center text-center">
+                    <a href="/" class="border-b border-b-slate-300 px-8 py-2 hover:text-slate-300 hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
+                        Home
+                    </a>
+                </div>
+                <div class="flex flex-col justify-center text-center">
+                    <a href="/games" class="border-b border-b-slate-300 px-8 py-2 hover:text-slate-300 hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
+                        Games
+                    </a>
+                </div>
+                @if(Auth::check())
+                    <div class="flex flex-col justify-center text-center">
+                        <a href="/profile" class="border-b border-b-slate-300 px-8 py-2 hover:text-slate-300 hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">Profile</a>
+                    </div>
+                @endif
+                @if(Auth::check())
+                    <div class="flex flex-col justify-center text-center">
+                        <a href="/logout" class="border-b border-b-slate-300 px-8 py-2 hover:text-slate-300	hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
+                            Logout
+                        </a>
+                    </div>
+                @else
+                    <div class="flex flex-col justify-center text-center">
+                        <a href="/login" class="border-b border-b-slate-300 px-8 py-2 hover:text-slate-300	hover:bg-gray-800 ease-linear duration-200 h-16 flex flex-col justify-center">
+                            Login
+                        </a>
+                    </div>
+                @endif
+            </div>
+        </div>
     </div>
 </div>
 <hr>
