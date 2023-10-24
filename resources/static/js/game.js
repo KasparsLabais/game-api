e179c14e610f1dfd9d452390df45f92606c0a493
kvanags <kvanags@firestormmedia.tv>
2023-10-24 11:30:05 +0100

    add https

--- resources/static/js/game.js
+++ resources/static/js/game.js
@@ -2,7 +2,7 @@ var playerList = {};
 var gameInstance = {};
 
 //var socket = io('http://localhost:3000');
-var socket = io('http://poker-bank.com');
+var socket = io('https://poker-bank.com');
 socket.on('connect', function(data) {
     console.log('connect', data);
     socket.emit('userconnected', {'username': window.username, 'id': window.id, 'avatar': window.avatar, 'playerToken' : window.playerToken });
