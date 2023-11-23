c103132ac0605c79e624bbfa27eb3452e0677f42
kvanags <kvanags@firestormmedia.tv>
2023-11-23 18:45:46 +0000

    fix modal

--- resources/views/components/ui/modal.blade.php
+++ resources/views/components/ui/modal.blade.php
@@ -1,10 +1,10 @@
 <div target="{{ $target }}" class="absolute game_modal-holder hidden">
-    <div target="{{ $target }}" class="game_modal-overlay fixed top-0 h-screen w-screen bg-gray-400 opacity-50">
+    <div target="{{ $target }}" class="game_modal-overlay fixed top-0 h-screen w-screen bg-gray-700 opacity-80">
     </div>
     <div target="{{ $target }}" class="game_modal-content fixed flex flex-col w-full h-full justify-center">
         <div class="flex flex-row w-full justify-center">
-            <div class="w-2/4 bg-gray-200 mt-4 shadow-md">
-                <div class="game_modal-header bg-gray-300 w-full px-4 py-4">
+            <div class="w-11/12 bg-gray-200 mt-4 shadow-md">
+                <div class="game_modal-header bg-gray-300 w-full px-4 py-4 text-center">
                 </div>
                 <div class="game_modal-body w-full px-4 py-4">
                 </div>
